66582d54fe86e51b85215833778eb627cb147f2f
Rubens P. Rosa <rubenspassos@hotmail.com>
2022-07-11 16:02:15 -0300

    Exercício finalizado

--- ex078.py
+++ ex078.py
@@ -1,21 +1,15 @@
 lista = []
-menor = 0
-maior = 0
 for cont in range(0,5):
     lista.append(int(input('Digite um valor: ')))
-    if cont == 0:
-        menor = lista[cont]
-        maior = lista[cont]
-        cont += 1
-    elif lista[cont] > maior:
-        maior = lista[cont]
-    elif lista[cont] < menor:
-        menor = lista[cont]
 print(f'Você digitou os valores{lista}')
+print(f'O maior valor digitado foi {max(lista)} na posição',end='')
 
-for c, maior in enumerate(lista):
-    print(f'O maior valor digitado foi {maior}')
-   # if maior == enumerate(lista):
-      #  print(f'O maior valor digitado foi {maior} nas posições {c}')
-print(menor)
+for c, l in enumerate(lista):
+    if max(lista) == lista[c]:
+        print(f' {c}...', end='')
+
+print(f'\nO menor valor digitado foi {min(lista)} na posição', end='')
+for c , l in enumerate(lista):
+    if min(lista) == lista[c]:
+        print(f' {c}...', end='')
 
