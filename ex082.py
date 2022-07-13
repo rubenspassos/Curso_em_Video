5d90efbd4badaa28dbee04e398302cd41e409ff9
Rubens P. Rosa <rubenspassos@hotmail.com>
2022-07-12 22:28:04 -0300

    Exercício Finalizado

---
+++ ex082.py
@@ -0,0 +1,23 @@
+lista = []
+listapar = []
+listaimpar = []
+while True:
+    lista.append(int(input('Digite um valor:')))
+    while True:
+        opção = str(input('Deseja continuar? [S/N] ')).upper().strip()[0]
+        if opção == 'N' or opção == 'S':
+            break
+        else:
+            print('Opção inválida, digite novamente: ')
+    if opção == 'N':
+        break
+for n in lista:
+    if n % 2 == 0:
+        listapar.append(n)
+for n in lista:
+    if n % 2 == 1:
+        listaimpar.append(n)
+print('-=' * 20)
+print(f'A lista compra é {lista}')
+print(f'A lista dos valores pares é {listapar}')
+print(f'A lista dos valores impares é {listaimpar}')
