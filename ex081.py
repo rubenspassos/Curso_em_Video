8082db8fb663540fc79e3272eccd771c3167b9c3
Rubens P. Rosa <rubenspassos@hotmail.com>
2022-07-12 22:02:57 -0300

    Exercício Finalizado

--- ex081.py
+++ ex081.py
@@ -1,15 +1,22 @@
 lista = []
+
 while True:
     lista.append(int(input("Digite um valor: ")))
     while True:
-        opção = str(input('Deseja continuar? [S/N]').upper().strip()[0])
+        opção = str(input('Deseja continuar? [S/N] ').upper().strip()[0])
         if opção == 'N' or opção == 'S':
             break
         else:
             print('Opção invalida digite novamente')
     if opção == 'N':
         break
+lista.sort(reverse=True)
 print(f'Voce digitou {len(lista)}  números')
-print(f'Os numeros digitados em ordem descrescente é {sort(lista)}')
+print(f'Os numeros digitados em ordem descrescente são {lista}')
 if 5 in lista:
-    print('O valor 5 foi digitado na lista')
+    print(f'O valor 5 foi digitado na lista nas posições', end='')
+    for c, l in enumerate(lista):
+        if 5 == lista[c]:
+            print(f' {c}...', end='')
+else:
+    print('O valor 5 não foi digitado')
