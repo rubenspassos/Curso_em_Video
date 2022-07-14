062a12ae44784ba89bf470981a36ae5541ac93ec
Rubens P. Rosa <rubenspassos@hotmail.com>
2022-07-14 15:19:28 -0300

    Exercício finalizado

--- ex038.py
+++ ex038.py
@@ -1,1 +1,8 @@
-
+n1 = int(input("digite o primeiro numero: "))
+n2 = int(input('Digite o segundo numero: '))
+if n1 == n2:
+    print('Os valores digitados são IGUAIS!')
+elif n1 > n2:
+    print(f'O PRIMEIRO valor é o maior!')
+else:
+    print(f'O SEGUNDO valor é o maior!')
