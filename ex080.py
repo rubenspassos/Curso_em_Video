019366f8f1a5a5cc5606d26963740400a80b31c1
Rubens P. Rosa <rubenspassos@hotmail.com>
2022-07-14 14:22:31 -0300

    Exercício finalizado

--- ex080.py
+++ ex080.py
@@ -2,15 +2,18 @@ lista = []
 cont = 1
 for cont in range (1,6):
     valor = int(input('Digite um Valor: '))
-    if cont == 1:
+    if cont == 1 or valor > lista[-1]:
         lista.append(valor)
         print('Valor inserido no final da lista...')
         cont += 1
-    if valor < lista:
-        lista [b -1] = valor
-    elif valor >= b:
-        lista [b + 1] = valor
-
-
-
-print(lista)
+    else:
+        pos = 0
+        while pos < len(lista):
+            if valor <= lista[pos]:
+                lista.insert(pos, valor)
+                print('Adicionado na primeira posição da lista' if pos == 0 else f'Adicionado '
+                                                                                 f'na posição {pos} da lista')
+                break
+            pos += 1
+print('-=' * 20)
+print(f'Os Valores digitados em orfem foram {lista}')
