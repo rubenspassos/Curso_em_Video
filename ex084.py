4a59156b176c505e4019d5299149a2adfab56fee
Rubens P. Rosa <rubenspassos@hotmail.com>
2022-07-14 23:06:27 -0300

    Exercício Finalizado

--- ex084.py
+++ ex084.py
@@ -1,9 +1,11 @@
 nome = list()
 dados = list()
+maior = cont = menor = 0
 while True:
     nome.append(str(input('Digite o nome: ')))
     nome.append(float(input('Digite o peso: ')))
     dados.append(nome[:])
+    nome.clear()
     while True:
         opção = str(input('Deseja Continuar? [S/N] ').upper().strip()[0])
         if opção == 'S' or opção == 'N':
@@ -12,13 +14,19 @@ while True:
             print('Opção invalida digite novamente ')
     if opção == 'N':
         break
-
 print(f'Ao todo você cadastrou {len(dados)} pessoas')
-maior = cont = menor 0
+nomemaior = ''
+nomemenor = ''
 for p in dados:
     if cont == 0:
-        maior = menor = p[1]
+        maior = p[1]
+        menor = p[1]
         cont += 1
     if p[1] >= maior:
-        print(f'{p[1]} é o maior peso, {p[0]}')
-    print(p[1])
+        maior = p[1]
+        nomemaior = p[0]
+    if p[1] <= menor:
+        menor = p[1]
+        nomemenor = p[0]
+print(f'O maior peso é {maior} de {nomemaior}')
+print(f'O menor peso é {menor} de {nomemenor}')
