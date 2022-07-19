806ad0708fae7afe297bc1e4c8baccfc592a8cde
Rubens P. Rosa <rubenspassos@hotmail.com>
2022-07-19 12:18:31 -0300

    Exercício Finalizado

--- ex086.py
+++ ex086.py
@@ -1,1 +1,19 @@
-for cont in range (0,9):
+matriz = []
+l1 = []
+l2 = []
+l3 = []
+for cont in range (0,3):
+    l1.append(int(input(f'Digite o valor [0,{cont}] ')))
+cont = 0
+for cont in range (0,3):
+    l2.append(int(input(f'Digite o valor [1,{cont}] ')))
+cont = 0
+for cont in range (0,3):
+    l3.append(int(input(f'Digite o valor [2,{cont}] ')))
+matriz.append([l1[:],l2[:],l3[:]])
+#print(matriz)
+print(f'[  {matriz[0][0][0]}  ][  {matriz[0][0][1]}  ][  {matriz[0][0][2]}  ]')
+print(f'[  {matriz[0][1][0]}  ][  {matriz[0][1][1]}  ][  {matriz[0][1][2]}  ]')
+print(f'[  {matriz[0][2][0]}  ][  {matriz[0][2][1]}  ][  {matriz[0][2][2]}  ]')
+
+
