c1d0d172ea0d8dbad4830e3aa4be19ac5f9f7f6d
Rubens P. Rosa <rubenspassos@hotmail.com>
2022-07-19 18:52:55 -0300

    Exercício Finalizado

---
+++ ex088.py
@@ -0,0 +1,24 @@
+
+from random import randint
+from time import sleep
+le = []
+print('-' * 40)
+print('JOGA NA MEGA SENA')
+print('-' * 40)
+op = int(input('Quantos jogos você quer que eu sorteie? '))
+print(f'SORTEANDO {op} JOGOS')
+for c in range(0,op):
+    li = []
+    while True:
+        r = randint(0,60)
+        if r not in li:
+            li.append(r)
+        if len(li) == 6:
+            break
+    le.append(li)
+for p in range (0,op):
+    print(f'Jogo {p+1}: {sorted(le[p])}')
+    sleep(1)
+
+
+
