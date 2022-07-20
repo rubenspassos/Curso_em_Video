66eacbe3cb2c87214ee6eb67d1c72008f38ffaaf
Rubens P. Rosa <rubenspassos@hotmail.com>
2022-07-20 11:53:43 -0300

    Exercício Finalizado

--- ex084.py
+++ ex084.py
@@ -1,6 +1,8 @@
+# Iniciando as listas e váriaveis
 nome = list()
 dados = list()
 maior = cont = menor = 0
+
 while True:
     nome.append(str(input('Digite o nome: ')))
     nome.append(float(input('Digite o peso: ')))
@@ -14,9 +16,11 @@ while True:
             print('Opção invalida digite novamente ')
     if opção == 'N':
         break
+#print(dados)
 print(f'Ao todo você cadastrou {len(dados)} pessoas')
-nomemaior = ''
-nomemenor = ''
+nomemaior = []
+nomemenor = []
+
 for p in dados:
     if cont == 0:
         maior = p[1]
@@ -24,9 +28,19 @@ for p in dados:
         cont += 1
     if p[1] >= maior:
         maior = p[1]
-        nomemaior = p[0]
+        nomemaior.append(p[0])
     if p[1] <= menor:
         menor = p[1]
-        nomemenor = p[0]
-print(f'O maior peso é {maior} de {nomemaior}')
-print(f'O menor peso é {menor} de {nomemenor}')
+        nomemenor.append(p[0])
+if dados[0][1] > menor: # Resolve o problema do primeiro nome lista nomemenor
+    del nomemenor[0]
+
+print(f'O maior peso foi de {maior}Kg. Peso de ', end = '')
+for c in range(0,len(nomemaior)):
+    print(f'{nomemaior[c]} ', end='')
+print()
+print(f'O menor peso foi de {menor}Kg. Peso de ', end = '')
+for c in range(0,len(nomemenor)):
+    print(f'{nomemenor[c]} ', end = '')
+print()
+
