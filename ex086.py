76b862f6654116a65bfea1b465558a6d5457a248
Rubens P. Rosa <rubenspassos@hotmail.com>
2022-07-20 17:19:33 -0300

    Exercício Finalizado

--- ex086.py
+++ ex086.py
@@ -16,4 +16,13 @@ print(f'[  {matriz[0][0][0]}  ][  {matriz[0][0][1]}  ][  {matriz[0][0][2]}  ]')
 print(f'[  {matriz[0][1][0]}  ][  {matriz[0][1][1]}  ][  {matriz[0][1][2]}  ]')
 print(f'[  {matriz[0][2][0]}  ][  {matriz[0][2][1]}  ][  {matriz[0][2][2]}  ]')
 
+'''matriz = [[0,0,0],[0,0,0],[0,0,0]]
+for l in range(0,3):
+    for c in range(0,3):
+        matriz[l][c] = int(input("Digite um valor para [{l},{c}]: '))
+
+for l in range(0,3):
+    for c in range(0,3):
+        print(f'[{matriz[l][c]:^5}]', end ='')'''
+
 
--- ex087.py
+++ ex087.py
@@ -12,18 +12,27 @@ acumulador = 0
 maior = 0
 soma = 0
 for n in range(0,9):
-    if n % 3 == 0 and n != 0:
+    if n % 3 == 0 and n != 0:# Pula linha no 3º espaço da lista
         print()
     print(f'[  {matriz[n]}  ]', end='')
     if matriz[n] % 2 == 0:
         acumulador += matriz[n]
-    if n > 5:
-        soma += matriz[n]
     if matriz[n] > maior and n > 2 and n < 6:
         maior = matriz[n]
+    if n % 3 == 0:
+        soma += matriz[n - 1]  # alimenta a soma caso o valor estiver na ultima coluna
+
 print()
 print('-=' * 20)
 print(f'A soma dos Valores Pares é {acumulador}')
 print(f'A Soma dos valores da Terceira coluna é {soma}')
 print(f'O maior valor da segunda linha é {maior}')
 
+'''matriz = [[0,0,0],[0,0,0],[0,0,0]]
+for l in range(0,3):
+    for c in range(0,3):
+        matriz[l][c] = int(input("Digite um valor para [{l},{c}]: '))
+
+for l in range(0,3):
+    for c in range(0,3):
+        print(f'[{matriz[l][c]:^5}]', end ='')'''
