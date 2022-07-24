15074770183a782137486e76753fb9a4918df706
Rubens P. Rosa <rubenspassos@hotmail.com>
2022-07-24 11:26:50 -0300

    Exercício Finalizado

--- ex090.py
+++ ex090.py
@@ -1,10 +1,15 @@
 aluno = dict()
 aluno['nome'] = str(input('Digite o nome do aluno: ').capitalize().strip())
-aluno['Média'] = float(input(f'Digite a média do aluno {aluno["nome"]}: '))
-if aluno['Média'] >= 5:
-    aluno['Situação'] = 'Aprovado'
+aluno['média'] = float(input(f'Digite a média do aluno {aluno["nome"]}: '))
+if aluno['média'] >= 7:
+    aluno['situação'] = 'Aprovado'
+elif 5 <= aluno['Média'] < 7:
+    aluno['situação'] = 'Recuperação'
 else:
-    aluno['Situação'] = 'Reprovado'
-print(f'Nome é igual a {aluno["nome"]}')
-print(f'A média é igual a {aluno["Média"]}')
-print(f'Situação é igual a {aluno["Situação"]}')
+    aluno['situação'] = 'Reprovado'
+#print(f'Nome é igual a {aluno["nome"]}')
+#print(f'A média é igual a {aluno["Média"]}')
+#print(f'Situação é igual a {aluno["Situação"]}')
+
+for ind, val in aluno.items():
+    print(f'{ind} é igual a {val}')
