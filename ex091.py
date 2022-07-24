ecc5c82bfe9552da7f52233ac90b21ff8c5b3c85
Rubens P. Rosa <rubenspassos@hotmail.com>
2022-07-24 12:02:30 -0300

    Exercício Finalizado

--- ex091.py
+++ ex091.py
@@ -1,7 +1,8 @@
 from random import randint
 from time import sleep
+#from operator import itemgetter
 jogadores = dict()
-rank = list()
+ranking = list()
 cont = 1
 for g in range(1,5):
     jogadores[f'Player {g}'] = randint(1,6)
@@ -13,5 +14,12 @@ for m,n in jogadores.items():
 print(f'{"RANKING DOS JOGADORES":=^30}')
 for i in sorted(jogadores, key=jogadores.get, reverse=True): #Organizando o dicionario por ordem crescente do valores
    print(f'O {cont}º lugar: {i} com {jogadores[i]} ')
+   sleep(1)
    cont += 1
 
+#Este modo també funciona, foi proposto na resolução do exercício
+# importando operador e itemgetter para criar lista ordenada.
+
+#ranking = sorted(jogadores.items() ,key=itemgetter(1), reverse=True)
+#for ind, val in enumerate(ranking):
+#    print(f'{ind+1}º lugar: {val[0]} com {val[1]}')
--- ex092.py
+++ ex092.py
@@ -1,11 +1,13 @@
+from datetime import datetime
 trabaiador = dict()
 trabaiador['nome'] = str(input('Nome: '))
-trabaiador['nascimento'] = int(input('Ano de Nascimento: '))
+nasc = int(input('Ano nascimento: '))
+trabaiador['idade'] = datetime.now().year - nasc
 trabaiador['carteira'] = int(input('Carteira de Trabalho (0 não tem): '))
 if trabaiador['carteira'] != 0:
     trabaiador['admissão'] = int(input('Ano de Contratação: '))
-    trabaiador['Salaraio'] = float(input('Salário: '))
-    trabaiador['aposentadoria'] = (trabaiador['admissão'] - trabaiador['nascimento']) + 35
+    trabaiador['salaraio'] = float(input('Salário: R$ '))
+    trabaiador['aposentadoria'] = trabaiador['idade'] + (trabaiador['admissão'] + 35) - datetime.now().year
 print(trabaiador)
 for n,m in trabaiador.items():
     print(f'{n} tem o valor {m}')
