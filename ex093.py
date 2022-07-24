798db23bccd090bf5f149e5f8b4ed9f3e88237fd
Rubens P. Rosa <rubenspassos@hotmail.com>
2022-07-24 12:15:25 -0300

    Exercício Finalizado

--- ex093.py
+++ ex093.py
@@ -1,20 +1,19 @@
 jogador = dict()
-partidas = soma = 0
+partidas = 0
 gols = list()
 jogador['nome'] = str(input('Nome do Jogador: '))
-partidas = int(input('Quantas partidas ele jogou? '))
+partidas = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
 for c in range(0,partidas):
     gols.append(int(input(f'Quantos gols na partida {c+1} ')))
-    soma += gols[c]
-jogador['gols'] = gols
-jogador['total'] = soma
+jogador['gols'] = gols[:]
+jogador['total'] = sum(gols)
 print('-=' * 30)
 print(jogador)
 print('-=' * 30)
 for n,m in jogador.items():
     print(f'O {n} tem o valor {m}')
 print('-=' * 30)
-print(f'O Jogaodor {jogador["nome"]} jogaou {partidas} partidas')
-for c, d in enumerate(gols):
+print(f'O Jogaodor {jogador["nome"]} jogou {len(jogador["gols"])} partidas')
+for c, d in enumerate(jogador["gols"]):
     print(f'=> Na partida {c}, ele fez {d} gols')
-print(f'Foi um total de {soma} gols')
+print(f'Foi um total de {jogador["total"]} gols')
